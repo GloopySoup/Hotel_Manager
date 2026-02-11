080081f3248f37f9ed19db4fdc65c0175b54f705
GloopySoup <tayabz7935@gmail.com>
2026-02-11 16:46:28 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from google import genai
 from sqlalchemy import Table,Column,MetaData,Text,Integer,create_engine
 
-client = genai.Client(api_key="AIzaSyCSdxVEU1WrNOyggB2gnyM3TUAzKSHv-EU")
+client = genai.Client(api_key="")
 
