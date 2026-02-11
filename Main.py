c6fd2c7aa7859fc2364a3097aefedb6befe2eb07
GloopySoup <tayabz7935@gmail.com>
2026-02-11 17:02:04 +0000

    client connects to api key from .env

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
+import os
 from google import genai
+from dotenv import load_dotenv, dotenv_values 
 from sqlalchemy import Table,Column,MetaData,Text,Integer,create_engine
+load_dotenv()
 
-client = genai.Client(api_key="")
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
