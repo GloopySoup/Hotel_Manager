bf53271f03a323a21e78f8383920e4fb935415a4
GloopySoup <tayabz7935@gmail.com>
2026-02-12 18:10:16 +0000

    create table added

--- Main.py
+++ Main.py
@@ -1,30 +1,39 @@
 import os
+import json
 from google import genai
 from dotenv import load_dotenv, dotenv_values 
-from sqlalchemy import Table,Column,MetaData,Text,String,Integer,create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
-from fastapi import FastAPI
+
 
 load_dotenv()
-app = FastAPI()
-
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-engine = create_engine(os.getenv("DATABASE_URL"))
+
+engine = create_engine("mysql://root:Red47red.@localhost:3306/hotelmanager")
+engine.connect()
 Session = sessionmaker(bind=engine)
-session = Session()
-
 Base = declarative_base()
+metadata = MetaData()
 
-#Generates content to fill the sql query to create a database table
-def generate_query():
+def create_hotel_table(user_input):
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
-        contents="Generate an sql query using sqlalchemy in python to create a table in mysql for my hotel called koalaHotels"
-    )
-    print(response.text)
+        contents="""Extract structured data from the text below.
 
+        Return ONLY valid JSON in this format:
+        {{
+        "hotel_name": "string",
+        "room_type": "string"
+        }}
+    Text: """ + user_input,
+    config={"response_mime_type": "application/json"})
 
+    data = json.loads(response.text)
+    hotel_name = data["hotel_name"]
+    hotel_table = Table(hotel_name,
+                        metadata,
+                        Column("customerID", Integer, primary_key=True))
+    metadata.create_all(engine)
 
-generate_query()
+create_hotel_table("Hey, i'd like to register with your hotel management system. We're called koala hotels and have 3 different types of Rooms: Large, Medium, Small")
